3f0eb909aa9aae0e52a234df203c8f5169956ada
dhea rizky <dhea.rizky@formulatrix.com>
2023-09-11 12:25:20 +0700

    Progressing the UI

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toogle class active
+const navbarNav = document.querySelector('.navbar-nav');
+//Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick=()=>{
+    navbarNav.classList.toggle('active');
+};
+
+//click out range form the sidebar for hiding it
+const hamburger = document.querySelector
+('#hamburger-menu');
+
+document.addEventListener('click', function(e) { 
+    if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target))
+    {
+        navbarNav.classList.remove('active');
+    }
+});
